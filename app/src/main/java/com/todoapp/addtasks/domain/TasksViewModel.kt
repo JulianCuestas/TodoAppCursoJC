d37e8988022f94ecb1618e365d4d8804d14be2fc
Julian Cuestas Jimenez <julian.cuestas@sii-group.co>
2023-04-19 22:15:47 -0500

    Proyecto base, creación de tareas en memoria, eliminación y edición

---
+++ app/src/main/java/com/todoapp/addtasks/domain/TasksViewModel.kt
@@ -0,0 +1,42 @@
+package com.todoapp.addtasks.domain
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.todoapp.addtasks.ui.model.TaskModel
+import javax.inject.Inject
+
+class TasksViewModel @Inject constructor(): ViewModel() {
+
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+    private val _tasksList = mutableStateListOf<TaskModel>()
+    val tasksList: List<TaskModel> = _tasksList
+
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated(task: String) {
+        this.onDialogClose()
+        this._tasksList.add(TaskModel(task = task))
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
+
+    fun onCheckBoxSelected(taskModel: TaskModel) {
+        val index = _tasksList.indexOf(taskModel)
+        _tasksList[index] = _tasksList[index].let {
+            it.copy(selected = !it.selected)
+        }
+    }
+
+    fun onItemRemove(taskModel: TaskModel) {
+        val task = _tasksList.find { it.id == taskModel.id }
+        _tasksList.remove(task)
+    }
+
+}
---
+++ app/src/main/java/com/todoapp/addtasks/ui/TasksScreen.kt
@@ -0,0 +1,137 @@
+package com.todoapp.addtasks.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.todoapp.addtasks.domain.TasksViewModel
+import com.todoapp.addtasks.ui.model.TaskModel
+
+@Composable
+fun TasksScreen(tasksViewModel: TasksViewModel) {
+
+    val showDialog: Boolean by tasksViewModel.showDialog.observeAsState(false)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(
+            show = showDialog,
+            onDismiss = { tasksViewModel.onDialogClose() },
+            onTaskAdded = { tasksViewModel.onTaskCreated(it) }
+        )
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TasksList(tasksViewModel)
+    }
+
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
+    FloatingActionButton(onClick = {
+        tasksViewModel.onShowDialogClick()
+    }, modifier = modifier.padding(16.dp)) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var myTask by remember {
+        mutableStateOf("")
+    }
+    if(show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)) {
+                Text(
+                    text = "Añade tu tarea", fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTask, onValueChange = { myTask = it},
+                    singleLine = true, maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                    myTask = ""
+                }, Modifier.fillMaxWidth()) {
+                    Text(text = "Añadir tarea")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasksList
+    LazyColumn {
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(taskModel = task, taskViewModel = tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, taskViewModel: TasksViewModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp).pointerInput(Unit) {
+                detectTapGestures(onLongPress = {
+                    taskViewModel.onItemRemove(taskModel)
+                })
+            },
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(text = taskModel.task, modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 4.dp))
+            Checkbox(checked = taskModel.selected, onCheckedChange = {
+                taskViewModel.onCheckBoxSelected(taskModel)
+            })
+        }
+    }
+}
